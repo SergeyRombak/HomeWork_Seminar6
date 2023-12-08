be87d27822706a6a6e04c5b64ef4b7c1691d72ec
Sergey Rombak <rombak.a87@mail.ru>
2023-12-08 18:49:05 +0500

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 2
+/* Задайте строку, содержащую латинские буквы в обоих регистрах. 
+Сформируйте строку, в которой все заглавные буквы заменены на строчные.*/
+
+public class Task2
+{
+public static void Main(string[] args)
+{
+string input = "aBcD1ef!-";
+// Преобразование всех заглавных букв в строчные
+string result = input.ToLower();
+// Вывод результата
+Console.WriteLine(result);
+}
+}
