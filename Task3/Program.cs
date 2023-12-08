b8ea479045107382d68e421772618bfb01021e59
Sergey Rombak <rombak.a87@mail.ru>
2023-12-08 19:06:20 +0500

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 3
+/* Задайте произвольную строку. Выясните, является ли она палиндромом.*/
+
+public class Task3
+{
+public static void Main(string[] args)
+{
+// Входная строка для проверки
+string input = "шалаш";
+bool isPalindrome = IsPalindrome(input);
+// Вывод результата
+Console.WriteLine(isPalindrome ? "Да" : "Нет");
+}
+public static bool IsPalindrome(string str)
+{
+// Нормализация строки путем удаления не буквенно-цифровыхсимволов и приведения к нижнему регистру
+string normalized = new
+string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+// Сравнение строки с ее перевернутым вариантом
+return normalized.SequenceEqual(normalized.Reverse());
+}
+}
